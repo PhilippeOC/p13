5e941e51c8df5b90ef9c7a25276ddcd53cd41b1f
philippe <philoufilou2@gmail.com>
2022-06-22 09:39:13 +0200

    Réduction dettes techniques - refactorisations

---
+++ p13/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from oc_lettings_site import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('lettings/', include('lettings.urls')),
+    path('profiles/', include('profiles.urls')),
+    path('admin/', admin.site.urls),
+]
